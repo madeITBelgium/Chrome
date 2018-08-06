2d62d8a032d3c7bea262fbabddf97092ec57adee
Made I.T <info@madeit.be>
2018-08-06 11:58:40 +0200

    #4 - Fix add extraCapabilities

--- src/ChromeBrowser.php
+++ src/ChromeBrowser.php
@@ -41,31 +41,35 @@ class ChromeBrowser
      */
     public function driver()
     {
-        if (!$this->mobile) {
-            $options = (new ChromeOptions())->addArguments([
-                '--disable-gpu',
-                '--headless',
-                '--log-path='.storage_path('logs/chromedriver-errors.log'),
-            ]);
-
-            return RemoteWebDriver::create(
-                'http://localhost:9515', DesiredCapabilities::chrome()->setCapability(
-                    ChromeOptions::CAPABILITY, $options
-                )
-            );
-        } else {
-            $ua = 'Mozilla/5.0 (iPhone; CPU iPhone OS 11_3 like Mac OS X) AppleWebKit/604.1.34 (KHTML, like Gecko) CriOS/67.0.3396.87 Mobile/15E216 Safari/604.1';
-            $capabilities = DesiredCapabilities::chrome();
-            $options = (new ChromeOptions())->addArguments([
-                '--disable-gpu',
-                '--headless',
-                '--log-path='.storage_path('logs/chromedriver-errors.log'),
-            ]);
-            $options->setExperimentalOption('mobileEmulation', ['userAgent' => $ua]);
-
-            return RemoteWebDriver::create(
-                'http://localhost:9515', $options->toCapabilities()
-            );
+        $ua = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/68.0.3440.75 Safari/537.36';
+        if ($this->mobile) {
+            $ua = 'Mozilla/5.0 (iPhone; CPU iPhone OS 11_4_1 like Mac OS X) AppleWebKit/604.1.34 (KHTML, like Gecko) CriOS/67.0.3396.87 Mobile/15G77 Safari/604.1';
         }
+        
+        $capabilities = DesiredCapabilities::chrome();
+        $options = (new ChromeOptions())->addArguments([
+            '--disable-gpu',
+            '--headless',
+            '--verbose',
+            '--user-agent="' . $ua . '"',
+            '--log-path='.storage_path('logs/chromedriver-errors.log'),
+        ]);
+        
+        if($this->mobile) {
+            $options->setExperimentalOption('mobileEmulation', ['userAgent' => $ua]);
+        }
+        
+        $capabilities = DesiredCapabilities::chrome()->setCapability(
+            ChromeOptions::CAPABILITY, $options
+        );
+
+        if($this->extraCapabilities !== null) {
+            foreach($this->extraCapabilities as $name => $value) {
+                $capabilities->setCapability($name, $value);
+            }
+        }
+        return RemoteWebDriver::create(
+            'http://localhost:9515', $capabilities
+        );
     }
 }
